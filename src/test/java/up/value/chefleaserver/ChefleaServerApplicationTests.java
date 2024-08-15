2c13f276f78cea7a5d2cab95af020ff525b1dcc3
Kim-TaeUk <xodnr0130@naver.com>
2024-08-15 19:01:52 +0900

    init; initial commit

---
+++ src/test/java/up/value/chefleaserver/ChefleaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package up.value.chefleaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChefleaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
