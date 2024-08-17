079a9d6ac0931a54df58d9a6092c46fc71cb7436
eundeang <111877048+eundeang@users.noreply.github.com>
2024-08-18 01:21:45 +0900

    feat; Popup 연관관계 추가
    OneToMany : PopupCategory, PopLike
    ManyToOne : UserRestaurant

--- src/main/java/up/value/chefleaserver/domain/Popup.java
+++ src/main/java/up/value/chefleaserver/domain/Popup.java
@@ -4,14 +4,22 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import lombok.AccessLevel;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 import static jakarta.persistence.GenerationType.IDENTITY;
 
 @Entity
+@Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Popup {
 
@@ -24,11 +32,19 @@ public class Popup {
 
     private String image;
 
-    private String address;
-
     private LocalDate period;
 
     private LocalDateTime startTime;
 
     private LocalDateTime endTime;
+
+    @OneToMany(mappedBy = "popup")
+    private List<PopupCategory> popupCategories = new ArrayList<>();
+
+    @OneToMany(mappedBy = "popup")
+    private List<PopupLike> popupLikes = new ArrayList<>();
+
+    @ManyToOne
+    @JoinColumn(name = "user_restaurant_id", nullable = false)
+    private UserRestaurant userRestaurant;
 }
