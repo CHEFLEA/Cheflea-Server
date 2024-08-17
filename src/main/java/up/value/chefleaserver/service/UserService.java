d6df4eb2e5f370fad91a7164031e5007f2657794
Kim-TaeUk <xodnr0130@naver.com>
2024-08-17 15:55:00 +0900

    feat; 서비스 레이어에 login 로직 추가

---
+++ src/main/java/up/value/chefleaserver/service/UserService.java
@@ -0,0 +1,30 @@
+package up.value.chefleaserver.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import up.value.chefleaserver.config.jwt.JwtProvider;
+import up.value.chefleaserver.config.jwt.UserAuthentication;
+import up.value.chefleaserver.domain.User;
+import up.value.chefleaserver.repository.UserRepository;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final JwtProvider jwtProvider;
+
+
+    public String login(Long userId) {
+        User user = getUserOrException(userId);
+        UserAuthentication userAuthentication = new UserAuthentication(user.getId(), null, null);
+        return jwtProvider.generateJWT(userAuthentication);
+    }
+
+    public User getUserOrException(Long userId) {
+        return userRepository.findById(userId)
+                .orElseThrow(() -> new RuntimeException("no user"));
+    }
+}
