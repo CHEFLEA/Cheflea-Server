5013af4832af63b287e8bf76ea604ffede3d6102
eundeang <111877048+eundeang@users.noreply.github.com>
2024-08-18 23:51:35 +0900

    feat; ReservationTableResponse의 팩토리 메서드 생성

--- src/main/java/up/value/chefleaserver/dto/ReservationTableResponse.java
+++ src/main/java/up/value/chefleaserver/dto/ReservationTableResponse.java
@@ -2,6 +2,8 @@ package up.value.chefleaserver.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import java.time.LocalDateTime;
+import java.util.List;
+import up.value.chefleaserver.domain.Popup;
 
 public record ReservationTableResponse(
         Long reservationTimeId,
@@ -10,4 +12,14 @@ public record ReservationTableResponse(
         @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "HH:mm")
         LocalDateTime reservationEndTime
 ) {
+    public static List<ReservationTableResponse> of(Popup popup) {
+        return popup.getPopupTimeTables()
+                .stream()
+                .map(timeTable -> new ReservationTableResponse(
+                        timeTable.getId(),
+                        timeTable.getStartTime(),
+                        timeTable.getEndTime()
+                ))
+                .toList();
+    }
 }
