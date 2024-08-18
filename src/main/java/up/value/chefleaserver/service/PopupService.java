c78c42c403b4db49dd5810e718a33123684205b5
eundeang <111877048+eundeang@users.noreply.github.com>
2024-08-18 19:43:50 +0900

    feat; 팝업 단건 조회 서비스 코드 구현

--- src/main/java/up/value/chefleaserver/service/PopupService.java
+++ src/main/java/up/value/chefleaserver/service/PopupService.java
@@ -4,8 +4,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import up.value.chefleaserver.domain.Popup;
-import up.value.chefleaserver.domain.PopupLike;
 import up.value.chefleaserver.domain.User;
+import up.value.chefleaserver.dto.HeadChefInfoResponse;
+import up.value.chefleaserver.dto.PopupDetailGetResponse;
 import up.value.chefleaserver.dto.PopupGetResponse;
 import up.value.chefleaserver.dto.PopupsGetResponse;
 import up.value.chefleaserver.repository.PopupRepository;
@@ -33,5 +34,10 @@ public class PopupService {
                 .toList();
 
         return PopupsGetResponse.of(popupGetResponses);
+    }
+
+    public PopupDetailGetResponse getPopup(User loginUser, Long popupId) {
+        Popup popup = popupRepository.findById(popupId).orElseThrow(RuntimeException::new);
+        return PopupDetailGetResponse.of(popup,HeadChefInfoResponse.of(popup),userService.isLikedByUser(loginUser, popup));
     }
 }
