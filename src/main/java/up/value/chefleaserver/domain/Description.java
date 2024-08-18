ef93bb1f83f172cdebbcc0ecd41ddd56090763c1
eundeang <111877048+eundeang@users.noreply.github.com>
2024-08-18 20:37:27 +0900

    style; optimize import

--- src/main/java/up/value/chefleaserver/domain/Description.java
+++ src/main/java/up/value/chefleaserver/domain/Description.java
@@ -1,5 +1,8 @@
 package up.value.chefleaserver.domain;
 
+import static jakarta.persistence.FetchType.LAZY;
+import static jakarta.persistence.GenerationType.IDENTITY;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
