c96c2a129e47dcadef7f7044ccd0f3613ddddc75
eundeang <111877048+eundeang@users.noreply.github.com>
2024-08-18 20:38:31 +0900

    style; optimize import

--- src/main/java/up/value/chefleaserver/domain/PopupCategory.java
+++ src/main/java/up/value/chefleaserver/domain/PopupCategory.java
@@ -1,5 +1,8 @@
 package up.value.chefleaserver.domain;
 
+import static jakarta.persistence.FetchType.LAZY;
+import static jakarta.persistence.GenerationType.IDENTITY;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -12,9 +15,6 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import up.value.chefleaserver.common.Category;
-
-import static jakarta.persistence.FetchType.LAZY;
-import static jakarta.persistence.GenerationType.IDENTITY;
 
 @Entity
 @Getter
