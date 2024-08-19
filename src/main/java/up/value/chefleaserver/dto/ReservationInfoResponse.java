4c07cc7ce59c0a1600da0e71a1748ad88fec1e3b
eundeang <111877048+eundeang@users.noreply.github.com>
2024-08-19 16:49:34 +0900

    feat; Response 형식에 맞추기 위해 @JsonFormat 사용

--- src/main/java/up/value/chefleaserver/dto/ReservationInfoResponse.java
+++ src/main/java/up/value/chefleaserver/dto/ReservationInfoResponse.java
@@ -1,12 +1,15 @@
 package up.value.chefleaserver.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import java.time.LocalDateTime;
 import up.value.chefleaserver.domain.UserPopup;
 
 public record ReservationInfoResponse(
         String reservationName,
         String reservationPhoneNumber,
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "HH:mm")
         LocalDateTime reservationStartTime,
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "HH:mm")
         LocalDateTime reservationEndTime,
         Long reservationTimeId,
         Integer reservationParty
