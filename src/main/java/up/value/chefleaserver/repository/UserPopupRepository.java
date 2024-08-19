431ea5a933b770ba949f5682d3445cfeca4d32da
eundeang <111877048+eundeang@users.noreply.github.com>
2024-08-19 19:13:43 +0900

    feat; UserPopupRepository에 findAllByUser 메소드 추가

--- src/main/java/up/value/chefleaserver/repository/UserPopupRepository.java
+++ src/main/java/up/value/chefleaserver/repository/UserPopupRepository.java
@@ -1,7 +1,10 @@
 package up.value.chefleaserver.repository;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import up.value.chefleaserver.domain.User;
 import up.value.chefleaserver.domain.UserPopup;
 
 public interface UserPopupRepository extends JpaRepository<UserPopup, Long> {
+    List<UserPopup> findAllByUser(User user);
 }
