9e6a34ac58ae25834eb25269a9df1138f117ba10
eundeang <111877048+eundeang@users.noreply.github.com>
2024-08-19 21:22:09 +0900

    feat; PopupInfosResponse에 팩토리 메서드 추가

--- src/main/java/up/value/chefleaserver/dto/PopupInfosResponse.java
+++ src/main/java/up/value/chefleaserver/dto/PopupInfosResponse.java
@@ -3,6 +3,9 @@ package up.value.chefleaserver.dto;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
+import up.value.chefleaserver.common.Category;
+import up.value.chefleaserver.domain.PopupCategory;
+import up.value.chefleaserver.domain.UserPopup;
 
 public record PopupInfosResponse(
         String popupImage,
@@ -14,4 +17,32 @@ public record PopupInfosResponse(
         LocalDateTime popupEndTime,
         String popupHeadChef
 ) {
+    public static PopupInfosResponse of(UserPopup userPopup) {
+        return new PopupInfosResponse(
+                userPopup.getPopup()
+                        .getImage(),
+                userPopup.getPopup()
+                        .getPopupCategories()
+                        .stream()
+                        .map(PopupCategory::getCategory)
+                        .map(Category::getKoreanLabel)
+                        .toList(),
+                userPopup.getPopup()
+                        .getName(),
+                userPopup.getPopup()
+                        .getUserRestaurant()
+                        .getRestaurant()
+                        .getAddress(),
+                userPopup.getPopup()
+                        .getPeriod(),
+                userPopup.getPopup()
+                        .getStartTime(),
+                userPopup.getPopup()
+                        .getEndTime(),
+                userPopup.getPopup()
+                        .getUserRestaurant()
+                        .getUser()
+                        .getName()
+        );
+    }
 }
