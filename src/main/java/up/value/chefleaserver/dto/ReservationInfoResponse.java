9403fc90193bd6a78c881efe486986d0970ed406
eundeang <111877048+eundeang@users.noreply.github.com>
2024-08-19 21:23:16 +0900

    feat; ReservationInfoResponse 팩토리 메서드 추가

--- src/main/java/up/value/chefleaserver/dto/ReservationInfoResponse.java
+++ src/main/java/up/value/chefleaserver/dto/ReservationInfoResponse.java
@@ -1,8 +1,17 @@
 package up.value.chefleaserver.dto;
 
+import up.value.chefleaserver.domain.UserPopup;
+
 public record ReservationInfoResponse(
         Long reservationId,
         PopupInfosResponse popupInfos,
         String reservationStatus
 ) {
+    public static ReservationInfoResponse of(UserPopup userPopup) {
+        return new ReservationInfoResponse(
+                userPopup.getId(),
+                PopupInfosResponse.of(userPopup),
+                userPopup.getStatus()
+        );
+    }
 }
