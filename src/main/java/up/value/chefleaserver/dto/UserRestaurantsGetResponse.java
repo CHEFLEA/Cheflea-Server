7f81a63b0f33053ca94fae7152ada099f2d50908
eundeang <111877048+eundeang@users.noreply.github.com>
2024-08-20 15:14:08 +0900

    feat; UserRestaurantsGetResponse 팩토리 메서드 추가

--- src/main/java/up/value/chefleaserver/dto/UserRestaurantsGetResponse.java
+++ src/main/java/up/value/chefleaserver/dto/UserRestaurantsGetResponse.java
@@ -1,8 +1,16 @@
 package up.value.chefleaserver.dto;
 
 import java.util.List;
+import up.value.chefleaserver.domain.UserRestaurant;
 
 public record UserRestaurantsGetResponse(
         List<UserRestaurantGetResponse> restaurants
 ) {
+    public static UserRestaurantsGetResponse of(List<UserRestaurant> userRestaurants) {
+        return new UserRestaurantsGetResponse(
+                userRestaurants.stream()
+                        .map(UserRestaurantGetResponse::of)
+                        .toList()
+        );
+    }
 }
