a6520d82112b0b1d91e1456ba65c5bc2fa436a0d
eundeang <111877048+eundeang@users.noreply.github.com>
2024-08-20 11:31:16 +0900

    refactor; DTO 생성 로직을 팩토리 메서드로 이동
    ReservationInfosResponse 팩토리 메서드 파라미터로 List<UserPopup>을 넣어 DTO 만드는 과정을 전부 팩토리 메서드 안으로 이동

--- src/main/java/up/value/chefleaserver/dto/ReservationInfosResponse.java
+++ src/main/java/up/value/chefleaserver/dto/ReservationInfosResponse.java
@@ -1,13 +1,17 @@
 package up.value.chefleaserver.dto;
 
 import java.util.List;
+import up.value.chefleaserver.domain.UserPopup;
 
 public record ReservationInfosResponse(
         List<ReservationInfoResponse> reservationInfos
 ) {
-    public static ReservationInfosResponse of(List<ReservationInfoResponse> reservationInfos) {
+    public static ReservationInfosResponse of(List<UserPopup> reservationInfos) {
         return new ReservationInfosResponse(
                 reservationInfos
+                        .stream()
+                        .map(ReservationInfoResponse::of)
+                        .toList()
         );
     }
 }
