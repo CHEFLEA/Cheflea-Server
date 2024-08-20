ac88020a7bc409b28ed8804edd7ca0a0c48a25e3
eundeang <111877048+eundeang@users.noreply.github.com>
2024-08-20 14:29:54 +0900

    feat;  saveRestaurant 메서드 구현
    예외 처리 추가할 것!

--- src/main/java/up/value/chefleaserver/service/RestaurantService.java
+++ src/main/java/up/value/chefleaserver/service/RestaurantService.java
@@ -3,9 +3,19 @@ package up.value.chefleaserver.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import up.value.chefleaserver.domain.Restaurant;
+import up.value.chefleaserver.domain.User;
+import up.value.chefleaserver.dto.RestaurantReservationRequest;
+import up.value.chefleaserver.repository.RestaurantRepository;
 
 @Service
 @Transactional
 @RequiredArgsConstructor
 public class RestaurantService {
+
+    private final RestaurantRepository restaurantRepository;
+
+    public void saveRestaurant(Restaurant restaurant) {
+        restaurantRepository.save(restaurant);
+    }
 }
