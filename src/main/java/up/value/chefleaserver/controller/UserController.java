df6c12ffac7cda50b0c61c896de4800a37e8a9a9
eundeang <111877048+eundeang@users.noreply.github.com>
2024-08-20 15:03:42 +0900

    feat; 엔드포인트 구성

--- src/main/java/up/value/chefleaserver/controller/UserController.java
+++ src/main/java/up/value/chefleaserver/controller/UserController.java
@@ -3,13 +3,18 @@ package up.value.chefleaserver.controller;
 
 import static org.springframework.http.HttpStatus.OK;
 
+import java.security.Principal;
 import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import up.value.chefleaserver.domain.User;
+import up.value.chefleaserver.dto.UserRestaurantsGetResponse;
+import up.value.chefleaserver.service.UserRestaurantService;
 import up.value.chefleaserver.service.UserService;
 
 @RestController
@@ -18,6 +23,7 @@ import up.value.chefleaserver.service.UserService;
 public class UserController {
 
     private final UserService userService;
+    private final UserRestaurantService userRestaurantService;
 
     @PostMapping("/login")
     public ResponseEntity<Map<String, String>> login(@RequestBody String userId) {
@@ -25,5 +31,13 @@ public class UserController {
         return ResponseEntity
                 .status(OK)
                 .body(Map.of("Authorization", token));
+    }
+
+    @GetMapping("/restaurants")
+    public ResponseEntity<UserRestaurantsGetResponse> getAllRegisteredRestaurant(Principal principal) {
+        User loginUser = userService.getUserOrException(Long.valueOf(principal.getName()));
+        return ResponseEntity
+                .status(OK)
+                .body(userRestaurantService.getAllRegisteredRestaurant(loginUser));
     }
 }
