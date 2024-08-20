896932103157f0e70338f7ca97c4ffd857c2319e
Kim-TaeUk <xodnr0130@naver.com>
2024-08-21 01:42:41 +0900

    feat; 요청 dto에 팝업 관련 request dto 매핑

---
+++ src/main/java/up/value/chefleaserver/dto/userRestaurant/UserRestaurantReservationRequest.java
@@ -0,0 +1,8 @@
+package up.value.chefleaserver.dto.userRestaurant;
+
+import up.value.chefleaserver.dto.popup.PopupRegisterPostRequest;
+
+public record UserRestaurantReservationRequest(
+        PopupRegisterPostRequest popupInfo
+) {
+}
