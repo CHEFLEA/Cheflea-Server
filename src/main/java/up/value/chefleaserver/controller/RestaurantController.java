96c8b765125285c17407491889332cfc8778d1d1
eundeang <111877048+eundeang@users.noreply.github.com>
2024-08-20 14:34:56 +0900

    feat; 엔드포인트 호출부 구현

--- src/main/java/up/value/chefleaserver/controller/RestaurantController.java
+++ src/main/java/up/value/chefleaserver/controller/RestaurantController.java
@@ -1,5 +1,7 @@
 package up.value.chefleaserver.controller;
 
+import static org.springframework.http.HttpStatus.CREATED;
+
 import java.security.Principal;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -7,20 +9,27 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import up.value.chefleaserver.domain.User;
 import up.value.chefleaserver.dto.RestaurantReservationRequest;
-import up.value.chefleaserver.service.RestaurantService;
+import up.value.chefleaserver.service.UserRestaurantService;
+import up.value.chefleaserver.service.UserService;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/restaurants")
 public class RestaurantController {
 
-    private final RestaurantService restaurantService;
+    private final UserRestaurantService userRestaurantService;
+    private final UserService userService;
 
     @PostMapping
     public ResponseEntity<Void> reserveRestaurant(Principal principal,
                                                   @RequestBody RestaurantReservationRequest request) {
-
+        User loginUser = userService.getUserOrException(Long.valueOf(principal.getName()));
+        userRestaurantService.reserveRestaurantOrThrow(loginUser, request);
+        return ResponseEntity
+                .status(CREATED)
+                .build();
     }
 
 }
