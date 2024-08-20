85c5740069f35e5db0741371c75873f3912c396e
eundeang <111877048+eundeang@users.noreply.github.com>
2024-08-20 14:33:37 +0900

    feat; Restaurant에서 LocalDateTime 사용하는 모든 필드 LocalTime으로 변경
    LocalDateTime은 날짜+시간으로 불필요한 정보를 가지게 될 뿐만 아니라, 리퀘스트 파싱할때 날짜가 오지 않아 따로 추가해줘야 함, 따라서  LocalDateTime 사용하는 모든 필드 LocalTime으로 변경

--- src/main/java/up/value/chefleaserver/domain/Restaurant.java
+++ src/main/java/up/value/chefleaserver/domain/Restaurant.java
@@ -10,8 +10,9 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
+import java.time.LocalTime;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -39,9 +40,9 @@ public class Restaurant {
 
     private LocalDate period;
 
-    private LocalDateTime startTime;
+    private LocalTime startTime;
 
-    private LocalDateTime endTime;
+    private LocalTime endTime;
 
     private String hall;
 
@@ -54,4 +55,23 @@ public class Restaurant {
     @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
+
+    @Builder
+    public Restaurant(String name, String image, String city, String district, String address, String detailedAddress,
+                      LocalDate period, LocalTime startTime, LocalTime endTime, String hall, String kitchen,
+                      String etc, User user) {
+        this.name = name;
+        this.image = image;
+        this.city = city;
+        this.district = district;
+        this.address = address;
+        this.detailedAddress = detailedAddress;
+        this.period = period;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.hall = hall;
+        this.kitchen = kitchen;
+        this.etc = etc;
+        this.user = user;
+    }
 }
