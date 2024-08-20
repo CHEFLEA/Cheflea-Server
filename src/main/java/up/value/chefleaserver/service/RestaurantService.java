28e15a6df3b2c739f70fad1abcac1aec1d79b06a
eundeang <111877048+eundeang@users.noreply.github.com>
2024-08-20 12:34:15 +0900

    feat; RestaurantService 클래스 생성

---
+++ src/main/java/up/value/chefleaserver/service/RestaurantService.java
@@ -0,0 +1,11 @@
+package up.value.chefleaserver.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class RestaurantService {
+}
