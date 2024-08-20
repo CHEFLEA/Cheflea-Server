8213d2334e2f071266b1e150df1f1f91e6084e5e
eundeang <111877048+eundeang@users.noreply.github.com>
2024-08-20 23:49:59 +0900

    rename; ReservationInfoResponse -> ReservationGetResponse로 이름 변경

--- src/main/java/up/value/chefleaserver/dto/ReservationGetResponse.java
+++ src/main/java/up/value/chefleaserver/dto/ReservationGetResponse.java
@@ -2,13 +2,13 @@ package up.value.chefleaserver.dto;
 
 import up.value.chefleaserver.domain.UserPopup;
 
-public record ReservationInfoResponse(
+public record ReservationGetResponse(
         Long reservationId,
         PopupInfosResponse popupInfos,
         String reservationStatus
 ) {
-    public static ReservationInfoResponse of(UserPopup userPopup) {
-        return new ReservationInfoResponse(
+    public static ReservationGetResponse of(UserPopup userPopup) {
+        return new ReservationGetResponse(
                 userPopup.getId(),
                 PopupInfosResponse.of(userPopup),
                 userPopup.getStatus()
--- src/main/java/up/value/chefleaserver/service/UserPopupService.java
+++ src/main/java/up/value/chefleaserver/service/UserPopupService.java
@@ -1,6 +1,5 @@
 package up.value.chefleaserver.service;
 
-import java.util.ArrayList;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -8,7 +7,6 @@ import org.springframework.transaction.annotation.Transactional;
 import up.value.chefleaserver.domain.Popup;
 import up.value.chefleaserver.domain.User;
 import up.value.chefleaserver.domain.UserPopup;
-import up.value.chefleaserver.dto.ReservationInfoResponse;
 import up.value.chefleaserver.dto.ReservationInfosResponse;
 import up.value.chefleaserver.dto.ReservationRequest;
 import up.value.chefleaserver.repository.UserPopupRepository;
