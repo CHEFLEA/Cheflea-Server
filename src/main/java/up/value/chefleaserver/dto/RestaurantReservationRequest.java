4b54d3a5502d82a8f510fc0893a653a6a21a47f5
eundeang <111877048+eundeang@users.noreply.github.com>
2024-08-20 14:35:53 +0900

    feat; LocalTime으로 변경하고 @JsonFormat 추가 + 레스토랑 이름 필드 추가

--- src/main/java/up/value/chefleaserver/dto/RestaurantReservationRequest.java
+++ src/main/java/up/value/chefleaserver/dto/RestaurantReservationRequest.java
@@ -1,17 +1,22 @@
 package up.value.chefleaserver.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.List;
 
 public record RestaurantReservationRequest(
         Location location,
+        String name,
         List<String> restaurantDescriptions,
         ToolDetail tools,
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yy.MM.dd")
         LocalDate desiredPeriod,
         String restaurantImage,
-        LocalDateTime desiredStartTime,
-        LocalDateTime desiredEndTime,
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "HH:mm")
+        LocalTime desiredStartTime,
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "HH:mm")
+        LocalTime desiredEndTime,
         Integer desiredPrice
 ) {
 }
