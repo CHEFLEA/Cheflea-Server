8c566d13682b54a149bd9dbc0afdd513db9df777
Kim-TaeUk <xodnr0130@naver.com>
2024-08-21 01:41:46 +0900

    feat; 쉐프 예약 등록 엔드포인트 추가

--- src/main/java/up/value/chefleaserver/controller/RestaurantController.java
+++ src/main/java/up/value/chefleaserver/controller/RestaurantController.java
@@ -5,12 +5,14 @@ import static org.springframework.http.HttpStatus.CREATED;
 import java.security.Principal;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import up.value.chefleaserver.domain.User;
 import up.value.chefleaserver.dto.RestaurantReservationRequest;
+import up.value.chefleaserver.dto.userRestaurant.UserRestaurantReservationRequest;
 import up.value.chefleaserver.service.UserRestaurantService;
 import up.value.chefleaserver.service.UserService;
 
@@ -32,4 +34,16 @@ public class RestaurantController {
                 .build();
     }
 
+    @PostMapping("/{restaurantId}/reservations")
+    public ResponseEntity<Void> registerUserRestaurantReservation(Principal principal,
+                                                                  @PathVariable("restaurantId") Long restaurantId,
+                                                                  @RequestBody UserRestaurantReservationRequest userRestaurantReservationRequest) {
+        User loginUser = userService.getUserOrException(Long.valueOf(principal.getName()));
+        userRestaurantService
+                .registerUserRestaurantReservation(restaurantId, loginUser, userRestaurantReservationRequest);
+
+        return ResponseEntity
+                .status(CREATED)
+                .build();
+    }
 }
