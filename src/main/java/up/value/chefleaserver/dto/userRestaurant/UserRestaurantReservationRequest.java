a41b7ff732fab7c636c5c17d2f31e2dbf8efa221
Kim-TaeUk <xodnr0130@naver.com>
2024-08-21 02:47:45 +0900

    feat; 요청 dto에 메뉴 관련 request dto 매핑

--- src/main/java/up/value/chefleaserver/dto/userRestaurant/UserRestaurantReservationRequest.java
+++ src/main/java/up/value/chefleaserver/dto/userRestaurant/UserRestaurantReservationRequest.java
@@ -1,8 +1,11 @@
 package up.value.chefleaserver.dto.userRestaurant;
 
+import java.util.List;
+import up.value.chefleaserver.dto.menu.MenuRegisterPostRequest;
 import up.value.chefleaserver.dto.popup.PopupRegisterPostRequest;
 
 public record UserRestaurantReservationRequest(
-        PopupRegisterPostRequest popupInfo
+        PopupRegisterPostRequest popupInfo,
+        List<MenuRegisterPostRequest> menus
 ) {
 }
