a90fa5434d1f3581a99d13b35bc9e4c189626492
Kim-TaeUk <xodnr0130@naver.com>
2024-08-24 16:11:50 +0900

    feat; 응답 dto에 restaurantDescription 관련 프로퍼티 매핑

--- src/main/java/up/value/chefleaserver/dto/restaurant/RestaurantInfoGetResponse.java
+++ src/main/java/up/value/chefleaserver/dto/restaurant/RestaurantInfoGetResponse.java
@@ -1,7 +1,9 @@
 package up.value.chefleaserver.dto.restaurant;
 
 import java.time.LocalDate;
+import java.util.List;
 import up.value.chefleaserver.domain.Restaurant;
+import up.value.chefleaserver.domain.RestaurantDescription;
 
 public record RestaurantInfoGetResponse(
         Long restaurantId,
@@ -9,18 +11,24 @@ public record RestaurantInfoGetResponse(
         String restaurantName,
         String restaurantAddress,
         LocalDate restaurantPeriod,
-        Integer price
+        Integer price,
+        List<String> restaurantDescription
 
 ) {
 
     public static RestaurantInfoGetResponse of(Restaurant restaurant) {
+        List<String> restaurantDescription = restaurant.getRestaurantDescriptions()
+                .stream()
+                .map(RestaurantDescription::getDescription)
+                .toList();
         return new RestaurantInfoGetResponse(
                 restaurant.getId(),
                 restaurant.getImage(),
                 restaurant.getName(),
                 restaurant.getAddress(),
                 restaurant.getPeriod(),
-                restaurant.getPrice()
+                restaurant.getPrice(),
+                restaurantDescription
         );
     }
 }
