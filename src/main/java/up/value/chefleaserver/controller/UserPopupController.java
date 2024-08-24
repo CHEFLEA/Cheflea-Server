047cf18321ac41ed6e762026e82935840076b35b
Kim-TaeUk <xodnr0130@naver.com>
2024-08-24 16:47:45 +0900

    feat; 쉐프의 예약 단건 조회 엔드포인트 추가

--- src/main/java/up/value/chefleaserver/controller/UserPopupController.java
+++ src/main/java/up/value/chefleaserver/controller/UserPopupController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import up.value.chefleaserver.domain.User;
 import up.value.chefleaserver.dto.ReservationGetResponse;
 import up.value.chefleaserver.dto.ReservationsGetResponse;
+import up.value.chefleaserver.dto.popup.ChefReservationGetResponse;
 import up.value.chefleaserver.service.UserPopupService;
 import up.value.chefleaserver.service.UserService;
 
@@ -40,5 +41,14 @@ public class UserPopupController {
                 .status(OK)
                 .body(userPopupService.getReservationInfo(reservationId));
     }
+
+    @GetMapping("/chef/{reservationId}")
+    public ResponseEntity<ChefReservationGetResponse> getChefReservation(Principal principal,
+                                                                         @PathVariable Long reservationId) {
+        return ResponseEntity
+                .status(OK)
+                .body(userPopupService.getChefReservation(reservationId));
+    }
+
 }
     
