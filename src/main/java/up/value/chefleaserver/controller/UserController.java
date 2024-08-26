0a1c8410b2af56f7f47edc1374f7c38c151b4c9e
Kim-TaeUk <xodnr0130@naver.com>
2024-08-26 17:10:12 +0900

    feat; 쉐프 등록을 위한 엔드포인트 추가

--- src/main/java/up/value/chefleaserver/controller/UserController.java
+++ src/main/java/up/value/chefleaserver/controller/UserController.java
@@ -1,6 +1,8 @@
 package up.value.chefleaserver.controller;
 
 
+import static org.springframework.http.HttpStatus.CREATED;
+import static org.springframework.http.HttpStatus.NO_CONTENT;
 import static org.springframework.http.HttpStatus.OK;
 
 import java.security.Principal;
@@ -9,12 +11,14 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import up.value.chefleaserver.domain.User;
 import up.value.chefleaserver.dto.PopupsGetResponse;
 import up.value.chefleaserver.dto.UserRestaurantsGetResponse;
+import up.value.chefleaserver.dto.user.RegisterUserRequest;
 import up.value.chefleaserver.service.PopupLikeService;
 import up.value.chefleaserver.service.UserRestaurantService;
 import up.value.chefleaserver.service.UserService;
@@ -43,12 +47,22 @@ public class UserController {
                 .status(OK)
                 .body(userRestaurantService.getAllRegisteredRestaurant(loginUser));
     }
-    
+
     @GetMapping("/favorites")
     public ResponseEntity<PopupsGetResponse> getPopupFavorites(Principal principal) {
         User loginUser = userService.getUserOrException(Long.valueOf(principal.getName()));
         return ResponseEntity
                 .status(OK)
                 .body(popupLikeService.getPopupFavorites(loginUser));
+    }
+
+    @PutMapping
+    public ResponseEntity<Void> registerUser(Principal principal,
+                                             @RequestBody RegisterUserRequest registerUserRequest) {
+        User loginUser = userService.getUserOrException(Long.valueOf(principal.getName()));
+        userService.registerUser(loginUser, registerUserRequest);
+        return ResponseEntity
+                .status(CREATED)
+                .build();
     }
 }
