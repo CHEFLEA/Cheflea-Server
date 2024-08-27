d0f2b1c9e1a8ce8df0bed6ab9096796fdc07769e
Kim-TaeUk <xodnr0130@naver.com>
2024-08-27 15:51:10 +0900

    feat; 레스토랑 이미지가 여러개 가능함에 따라 RestaurantImage 엔티티 추가

---
+++ src/main/java/up/value/chefleaserver/domain/RestaurantImage.java
@@ -0,0 +1,31 @@
+package up.value.chefleaserver.domain;
+
+import static jakarta.persistence.FetchType.LAZY;
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class RestaurantImage {
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    @Column(name = "restaurant_image_id")
+    private Long id;
+
+    String imageUrl;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "restaurant_id", nullable = false)
+    Restaurant restaurant;
+}
