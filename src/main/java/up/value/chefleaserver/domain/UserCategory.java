b3ce8e5812465566f20c2c1638af7d582c06c9d1
Kim-TaeUk <xodnr0130@naver.com>
2024-08-27 21:33:59 +0900

    feat; UserCategory에 @Getter 추가

--- src/main/java/up/value/chefleaserver/domain/UserCategory.java
+++ src/main/java/up/value/chefleaserver/domain/UserCategory.java
@@ -12,10 +12,12 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import lombok.AccessLevel;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import up.value.chefleaserver.common.Category;
 
 @Entity
+@Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class UserCategory {
 
