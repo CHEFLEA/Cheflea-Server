acd6391010c3975f75882cde4a9ff9efe97a8a6d
eundeang <111877048+eundeang@users.noreply.github.com>
2024-08-29 20:36:35 +0900

    feat; 레스토랑 좋아요 기능 서비스 코드 구현

--- src/main/java/up/value/chefleaserver/service/RestaurantLikeService.java
+++ src/main/java/up/value/chefleaserver/service/RestaurantLikeService.java
@@ -1,9 +1,12 @@
 package up.value.chefleaserver.service;
 
 import java.util.List;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import up.value.chefleaserver.domain.Restaurant;
 import up.value.chefleaserver.domain.RestaurantLike;
 import up.value.chefleaserver.domain.User;
 import up.value.chefleaserver.dto.restaurant.RestaurantGetResponse;
@@ -16,6 +19,7 @@ import up.value.chefleaserver.repository.RestaurantLikeRepository;
 public class RestaurantLikeService {
 
     private final UserService userService;
+    private final RestaurantService restaurantService;
     private final RestaurantLikeRepository restaurantLikeRepository;
 
     @Transactional(readOnly = true)
@@ -25,5 +29,18 @@ public class RestaurantLikeService {
                 .map(restaurantLike -> RestaurantGetResponse.of(restaurantLike.getRestaurant(),
                         userService.isLikedByUser(loginUser, restaurantLike.getRestaurant()))).toList();
         return RestaurantsGetResponse.of(restaurantGetResponses);
+    }
+
+    public HttpStatus registerOrDeleteRestaurantLike(User loginUser, Long restaurantId) {
+        Restaurant restaurant = restaurantService.findByRestaurantOrThrow(restaurantId);
+        Optional<RestaurantLike> restaurantLike = restaurantLikeRepository.findByRestaurantAndUser(restaurant,
+                loginUser);
+        if (restaurantLike.isPresent()) {
+            restaurantLikeRepository.delete(restaurantLike.get());
+            return HttpStatus.NO_CONTENT;
+        } else {
+            restaurantLikeRepository.save(RestaurantLike.create(loginUser, restaurant));
+            return HttpStatus.CREATED;
+        }
     }
 }
