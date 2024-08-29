78cb19f7d660e94b7489caec4cf6ba7c51a0e2a7
eundeang <111877048+eundeang@users.noreply.github.com>
2024-08-29 15:23:16 +0900

    feat; RestaurantLikeRepository에  findByRestaurantAndUser(Restaurant restaurant, User user); 메서드 추가

--- src/main/java/up/value/chefleaserver/repository/RestaurantLikeRepository.java
+++ src/main/java/up/value/chefleaserver/repository/RestaurantLikeRepository.java
@@ -1,10 +1,14 @@
 package up.value.chefleaserver.repository;
 
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import up.value.chefleaserver.domain.Restaurant;
 import up.value.chefleaserver.domain.RestaurantLike;
 import up.value.chefleaserver.domain.User;
 
 public interface RestaurantLikeRepository extends JpaRepository<RestaurantLike, Long> {
     List<RestaurantLike> findAllByUser(User user);
+
+    Optional<RestaurantLike> findByRestaurantAndUser(Restaurant restaurant, User user);
 }
